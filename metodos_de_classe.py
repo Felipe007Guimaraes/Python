965e5cfb64185aa6af0e23054187335422898833
Felipe007Guimaraes <felipe007.dnl123@gmail.com>
2022-03-05 01:26:42 -0300

    Estudo POO

--- metodos_de_classe.py
+++ metodos_de_classe.py
@@ -1,3 +1,4 @@
+from random import randint
 class pessoa:
     ano_atual = 2022
 
@@ -12,9 +13,14 @@ class pessoa:
     def por_ano_nascimento(cls, nome, ano_nascimento):
         idade = cls.ano_atual - ano_nascimento
         return cls(nome, idade)
-
+    
+    @staticmethod
+    def gera_id():
+        rand = randint(10000,19999)
+        return rand
 
 p1 = pessoa('Felipe', 27)
 print(p1.nome, p1.idade)
 p1.get_ano_nascimento()
 print(p1)
+print(pessoa.gera_id())
